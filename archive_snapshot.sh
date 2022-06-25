5709e7570adcf9423ce7e5ba99049ba209707694
Erica Young <27747481+EricaPomme@users.noreply.github.com>
2022-06-25 08:29:38 -0700

    Update archive_snapshot.sh

--- archive_snapshot.sh
+++ archive_snapshot.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 MIN_SNAPSHOTS=3     # Minimum number of snapshots to keep
-SPACE_THRESHOLD=75  # Clear oldest snapshots if disk usage is above this percentage
+SPACE_THRESHOLD=90  # Clear oldest snapshots if disk usage is above this percentage
 
 SRC='/mnt/images/.snapshots'    # Source directory
-DEST='/mnt/disk5/.snapshots'    # Destination directory
+DEST='/mnt/disk8/.snapshots'    # Destination directory
 SNAPSHOT_SEARCH_REGEX='^\/mnt\/images\/\.snapshots\/[0-9]{8}$'  # Regex to match snapshots in SRC. Mine are all in the format: /mnt/images/.snapshots/YYYYMMDD
 
-for i in $(find $SRC -regextype posix-extended -regex $SNAPSHOT_SEARCH_REGEX); do    
+for i in $(find $SRC -regextype posix-extended -regex $SNAPSHOT_SEARCH_REGEX); do
+    #Clearing based on snapshot size currently doesn't work because btrfs subvolume delete removes the handle for the snapshot before freeing the space, so it thinks the disk is still full. This leads to all snapshots up to the limit being killed off. Oops.
+    #while [ $(df $DEST | tail -n 1 | awk '{print $4}') -lt $(du -s $SRC | awk '{print $1}') ]; do
     while [ $(df -h $DEST | tail -n 1 | awk '{print $5}' | sed 's/%//') -gt $SPACE_THRESHOLD ]; do
         if [ $(ls $DEST | wc -l) -gt $MIN_SNAPSHOTS ]; then
-            $oldest=$DEST+'/'+$(ls -t $DEST | tail -n 1)
-            btrfs subvolume delete $oldest
+            btrfs subvolume delete "$DEST/$(ls -t $DEST | tail -n 1)"
         else 
             echo "Not enough snapshots to clear. Cannot delete $i. Unable to archive further snapshots."
             exit 1
